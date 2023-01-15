ae699a3485c13ef4b3b97ddfcf3cd9ab449ce82b
Emmanuel Genard <edgenard@gmail.com>
2023-01-15 20:47:24 +0100

    add request validator for search endpoint

---
+++ load-test.mjs
@@ -0,0 +1,27 @@
+
+
+const sleep = milliseconds =>
+new Promise(resolve =>
+  setTimeout(() => {resolve(console.log("Sleeping...")) }, milliseconds)
+  );
+
+async function fetchResults() {
+  const response = await fetch("https://zo5o8f92bh.execute-api.us-east-1.amazonaws.com/dev/restaurants/search", {
+    "headers": {
+      "content-type": "application/json",
+      "Referrer-Policy": "strict-origin-when-cross-origin"
+    },
+    "body": "{\"theme\":\"cartoon\"}",
+    "method": "POST"
+  })
+  const body = await response.json()
+  console.log(body);
+}
+
+for (let index = 0; index < 1000; index++) {
+
+    await fetchResults()
+    await sleep(10)
+}
+
+console.log("ALL DONE")
