da830fb77d613ccdc2d08e760bb8e444d83e4025
Emmanuel Genard <edgenard@gmail.com>
2023-01-17 12:19:54 +0100

    lint seed-restaurants.js file

--- seed-restaurants.js
+++ seed-restaurants.js
@@ -5,46 +5,46 @@ require('dotenv').config()
 
 const restaurants = [
   {
-    name: "Fangtasia",
-    image: "https://d2qt42rcwzspd6.cloudfront.net/manning/fangtasia.png",
-    themes: ["true blood"]
+    name: 'Fangtasia',
+    image: 'https://d2qt42rcwzspd6.cloudfront.net/manning/fangtasia.png',
+    themes: ['true blood']
   },
   {
     name: "Shoney's",
     image: "https://d2qt42rcwzspd6.cloudfront.net/manning/shoney's.png",
-    themes: ["cartoon", "rick and morty"]
+    themes: ['cartoon', 'rick and morty']
   },
   {
     name: "Freddy's BBQ Joint",
     image: "https://d2qt42rcwzspd6.cloudfront.net/manning/freddy's+bbq+joint.png",
-    themes: ["netflix", "house of cards"]
+    themes: ['netflix', 'house of cards']
   },
   {
-    name: "Pizza Planet",
-    image: "https://d2qt42rcwzspd6.cloudfront.net/manning/pizza+planet.png",
-    themes: ["netflix", "toy story"]
+    name: 'Pizza Planet',
+    image: 'https://d2qt42rcwzspd6.cloudfront.net/manning/pizza+planet.png',
+    themes: ['netflix', 'toy story']
   },
   {
-    name: "Leaky Cauldron",
-    image: "https://d2qt42rcwzspd6.cloudfront.net/manning/leaky+cauldron.png",
-    themes: ["movie", "harry potter"]
+    name: 'Leaky Cauldron',
+    image: 'https://d2qt42rcwzspd6.cloudfront.net/manning/leaky+cauldron.png',
+    themes: ['movie', 'harry potter']
   },
   {
     name: "Lil' Bits",
-    image: "https://d2qt42rcwzspd6.cloudfront.net/manning/lil+bits.png",
-    themes: ["cartoon", "rick and morty"]
+    image: 'https://d2qt42rcwzspd6.cloudfront.net/manning/lil+bits.png',
+    themes: ['cartoon', 'rick and morty']
   },
   {
-    name: "Fancy Eats",
-    image: "https://d2qt42rcwzspd6.cloudfront.net/manning/fancy+eats.png",
-    themes: ["cartoon", "rick and morty"]
+    name: 'Fancy Eats',
+    image: 'https://d2qt42rcwzspd6.cloudfront.net/manning/fancy+eats.png',
+    themes: ['cartoon', 'rick and morty']
   },
   {
-    name: "Don Cuco",
-    image: "https://d2qt42rcwzspd6.cloudfront.net/manning/don%20cuco.png",
-    themes: ["cartoon", "rick and morty"]
-  },
-];
+    name: 'Don Cuco',
+    image: 'https://d2qt42rcwzspd6.cloudfront.net/manning/don%20cuco.png',
+    themes: ['cartoon', 'rick and morty']
+  }
+]
 
 const putReqs = restaurants.map(x => ({
   PutRequest: {
@@ -58,5 +58,5 @@ const req = {
   }
 }
 dynamodb.batchWrite(req).promise()
-  .then(() => console.log("all done"))
+  .then(() => console.log('all done'))
   .catch(err => console.error(err))
