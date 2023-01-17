43923759639f94e1d78c3c7aa51dc1802da83295
Emmanuel Genard <edgenard@gmail.com>
2023-01-17 13:29:22 +0100

    Update load test script to use authorized user

--- load-test.mjs
+++ load-test.mjs
@@ -1,4 +1,4 @@
-
+import given from './tests/steps/given.js'
 const sleep = milliseconds =>
   new Promise(resolve =>
     setTimeout(() => { resolve(console.log('Sleeping...')) }, milliseconds)
@@ -6,12 +6,13 @@ const sleep = milliseconds =>
 
 const searchTerm = process.argv.slice(2)[0] || 'cartoon'
 const timeOut = parseInt(process.argv.slice(2)[1]) || 10
-
+const user = await given.an_authenticated_user()
 async function fetchResults (searchTerm) {
   const response = await fetch('https://zo5o8f92bh.execute-api.us-east-1.amazonaws.com/dev/restaurants/search', {
     headers: {
       'content-type': 'application/json',
-      'Referrer-Policy': 'strict-origin-when-cross-origin'
+      'Referrer-Policy': 'strict-origin-when-cross-origin',
+      Authorization: user.idToken
     },
     body: `{"theme": "${searchTerm}"}`,
     method: 'POST'
@@ -20,7 +21,7 @@ async function fetchResults (searchTerm) {
   console.log(body)
 }
 
-for (let index = 0; index < 10000; index++) {
+for (let index = 0; index < 10; index++) {
   await fetchResults(searchTerm)
   // fetchResults(searchTerm)
   // await sleep(timeOut)
